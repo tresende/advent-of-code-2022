cce7a3b935b9a12792e53c56ffd0d98f33e83172
Thiago Resende <thiago.gcresende@gmail.com>
2022-12-03 18:17:36 -0300

    refactor: Refactor compose

--- src/utils/compose.ts
+++ src/utils/compose.ts
@@ -1,10 +1,8 @@
-export const compose = (...fns) =>
-  function (arg) {
-    return fns.reduce(async (acc, fn) => {
-      if (Promise.resolve(acc) === acc) {
-        return fn(await acc)
-      } else {
-        return fn(acc)
-      }
-    }, arg)
-  }
+export const compose =
+  (...fns) =>
+  (arg) =>
+    fns.reduce(
+      async (acc, fn) =>
+        Promise.resolve(acc) === acc ? fn(await acc) : fn(acc),
+      arg
+    )
